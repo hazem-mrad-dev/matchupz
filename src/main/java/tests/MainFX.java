3d56381caa9175a636162107963bcfe22091ee9b
ibtissem <ibtissem.benamara@esprit.tn>
2025-02-20 15:38:07 +0100

    CRUD-inputValidation

--- src/main/java/tests/MainFX.java
+++ src/main/java/tests/MainFX.java
@@ -9,33 +9,26 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainFX extends Application {
-    private static Stage primaryStage;
-
-    @Override
-    public void start(Stage stage) {
-        primaryStage = stage;
-        loadScene("AjoutFournisseur.fxml");
-
-        stage.setTitle("Gestion des Fournisseurs et Matériel");
-        stage.show();
-    }
-
-    // Load new scene dynamically
-    public static void loadScene(String fxmlFile) {
-        try {
-            FXMLLoader loader = new FXMLLoader(MainFX.class.getResource("/" + fxmlFile));
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-
-            primaryStage.setScene(scene);
-            primaryStage.centerOnScreen();
-        } catch (IOException e) {
-            System.out.println("Erreur de chargement du FXML : " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/AjoutFournisseur.fxml"));
+        try {
+            Parent root = loader.load();
+
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Ajouter");
+            primaryStage.show();}
+        catch (RuntimeException | IOException r){
+            System.out.println(r.getMessage());
+        }
+
     }
 }
